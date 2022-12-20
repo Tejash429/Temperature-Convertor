0c4cca40be7aeb89b47021046a1bc5900a860b9c
Tejash Patel <110290355+Tejash429@users.noreply.github.com>
2022-12-20 22:37:08 +0530

    Add files via upload
    Uploaded files

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const celcius = document.querySelector('#celcius');
+const fahrenheit = document.querySelector('#fahrenheit');
+const kelvin = document.querySelector('#kelvin');
+const inputs = document.getElementsByClassName('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  let input = inputs[i];
+
+  input.addEventListener('input', function (e) {
+    let value = parseFloat(e.target.value);
+
+    switch (e.target.name) {
+      case 'celcius':
+        fahrenheit.value = celcius.value * 1.8 + 32;
+        kelvin.value = celcius.value + 273.15;
+        break;
+      case 'fahrenheit':
+        celcius.value = (fahrenheit.value - 32) / 1.8;
+        kelvin.value = (fahrenheit.value - 32) / 18 + 273.15;
+        break;
+      case 'kelvin':
+        celcius.value = kelvin.value - 273.15;
+        fahrenheit.value = (kelvin.value - 273.15) * 1.8 + 32;
+        break;
+    }
+  });
+}
