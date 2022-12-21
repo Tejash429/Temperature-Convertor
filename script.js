ce47c454745e5c26969660f48ab455a9986f180d
Tejash Patel <pateltejash429@gmail.com>
2022-12-22 01:19:50 +0530

    Added Copy

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const celcius = document.querySelector('#celcius');
+const celcius = document.getElementById('celcius');
 const fahrenheit = document.querySelector('#fahrenheit');
 const kelvin = document.querySelector('#kelvin');
 const inputs = document.getElementsByClassName('input');
+
+const copyBtn1 = document.querySelector('.copyText1');
+const copyBtn2 = document.querySelector('.copyText2');
+const copyBtn3 = document.querySelector('.copyText3');
 
 for (let i = 0; i < inputs.length; i++) {
   let input = inputs[i];
@@ -13,29 +17,46 @@ for (let i = 0; i < inputs.length; i++) {
 
     switch (e.target.name) {
       case 'celcius':
-        fahrenheit.value = +celcius.value * 1.8 + 32;
-        kelvin.value = +celcius.value + 273.15;
-        if (celcius.value === '') {
+        fahrenheit.value = value * 1.8 + 32;
+        kelvin.value = value + 273.15;
+        if (value === '') {
           fahrenheit.value = '';
           kelvin.value = '';
         }
+
         break;
+
       case 'fahrenheit':
-        celcius.value = (+fahrenheit.value - 32) / 1.8;
-        kelvin.value = (+fahrenheit.value - 32) / 18 + 273.15;
-        if (fahrenheit.value === '') {
+        celcius.value = (value - 32) / 1.8;
+        kelvin.value = (value - 32) / 18 + 273.15;
+        if (value === '') {
           celcius.value = '';
           kelvin.value = '';
         }
         break;
+
       case 'kelvin':
-        celcius.value = +kelvin.value - 273.15;
-        fahrenheit.value = (+kelvin.value - 273.15) * 1.8 + 32;
-        if (kelvin.value === '') {
+        celcius.value = value - 273.15;
+        fahrenheit.value = (value - 273.15) * 1.8 + 32;
+        if (value === '') {
           fahrenheit.value = '';
           celcius.value = '';
         }
         break;
     }
+    copyBtn1.onclick = () => {
+      celcius.select();
+      navigator.clipboard.writeText(celcius.value);
+    };
+
+    copyBtn2.onclick = () => {
+      fahrenheit.select();
+      navigator.clipboard.writeText(fahrenheit.value);
+    };
+
+    copyBtn3.onclick = () => {
+      kelvin.select();
+      navigator.clipboard.writeText(kelvin.value);
+    };
   });
 }
